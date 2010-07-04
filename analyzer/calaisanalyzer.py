1a50b0f4d62168837d9cb5336b6ce8f126a404c2
typpo <ian@typpo.us>
2010-07-04 03:44:01 +0000

    opencalais working

--- analyzer/calaisanalyzer.py
+++ analyzer/calaisanalyzer.py
@@ -1,5 +1,8 @@
 from baseanalyzer import BaseAnalyzer
 from .lib.calais import Calais
+from ConfigParser import ConfigParser
+
+CONFIG_PATH = 'analyzer/calais.cfg'
 
 class CalaisAnalyzer(BaseAnalyzer):
 	"""Class for sending database articles to OpenCalais
@@ -8,8 +11,23 @@ class CalaisAnalyzer(BaseAnalyzer):
 		"""Initialize with database connection"""
                 super(CalaisAnalyzer, self).__init__(conn)
                 print 'Initializing CalaisAnalyzer...'
+		self.loadkey()
+		self.calais = Calais(self.key, submitter='gildedage')
+
+	def loadkey(self):
+                config = ConfigParser()
+                config.readfp(open(CONFIG_PATH))
+		self.key = config.get('config', 'key')
 
 	def execute(self, documents):
 		print 'Running OpenCalais analysis...'
-		for document in documents:
-			pass
+		for doc in documents:
+			title = doc[4]
+			print
+			print 'ARTICLE:', title
+			article = doc[6]
+			if not article is None:
+				result = self.calais.analyze(article)
+				result.print_topics()
+				result.print_relations()
+				result.print_entities()
