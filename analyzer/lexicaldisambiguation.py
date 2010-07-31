da1eea34eba73622f0bd9b24a902b110fc88a3be
typpo <ian@typpo.us>
2010-07-31 21:11:06 +0000

    starting lexical disambiguation

---
+++ analyzer/lexicaldisambiguation.py
@@ -0,0 +1,27 @@
+
+
+class LexicalDisambiguator:
+    """Basic tool for extracted entity disambiguation that uses basic
+    string-based rules.
+
+    eg. The following should resolve to the same entity:
+        LINCOLN, Lincoln, Abe Lincoln
+        Washington, Washington DC, Washington D.C.
+
+    TODO would be really nice to use FTS3.
+    """
+    def __init__(self, conn):
+        self.conn = conn
+
+    def disambiguate(pairs):
+        """Takes type-value pairs.
+        eg. Person-Lincoln
+        """
+
+        # Look for similar matches with both type AND value
+        # This is a high probability match.
+
+        
+
+        # Look for similar matches with just value
+        # Lower probability match
