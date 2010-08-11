01520f5c391cbb0677ed9da48bb6407bed4d521c
Ian <ian@typpo.us>
2010-08-10 22:24:29 -0400

    fts util done

--- fts.py
+++ fts.py
@@ -1,13 +1,18 @@
 import db.articles
 
 def populateFts(conn):
-    """Populates FTS table with contents of articles table."""
+    """Populates FTS table with contents of a normal articles table."""
     cur = conn.cursor()
     cur.execute('SELECT * FROM articles')
     rs = cur.fetchall()
     articles = db.articles.processAll(rs)
 
+    print 'Populating database with', len(articles), 'articles'
     for a in articles:
-        stmt = 'INSERT INTO articles_fts VALUES (null, ?, ?, ?, ?, ?, ?, ?, datetime(?), datetime(?))'
-        args = (a.id, a.source, a.alignment, a.page, a.title, a.summary, a.text, a.url, a.articleDate, a.timeEnter)
+        stmt = 'INSERT INTO articles_fts VALUES (?, ?, ?, ?, ?, ?, ?, ?, datetime(?), datetime(?))'
+        args = (a.id, a.source, a.alignment, a.page, a.title, a.summary, \
+                a.text, a.url, a.articleDate, a.timeEnter)
         cur.execute(stmt, args)
+
+    conn.commit()
+    print 'Done.'
